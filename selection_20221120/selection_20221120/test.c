b0c1cf3360341ab934bb150340f4e54edddcafeb
xiaobanban2020 <panmz1990@126.com>
2022-11-20 22:40:32 +0800

    add selection and while

---
+++ selection_20221120/selection_20221120/test.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//main()
+//{
+//	int input = 0;
+//	printf("加入比特");
+//	printf("你要好好学习吗(1/0)？>:");
+//	scanf("%d", &input);
+//	if (input == 1)
+//	{
+//		printf("好offer\n");
+//	}
+//	else
+//	{
+//		printf("卖红薯\n");
+//	}
+//	return 0;
+//}
+
+//比特 - 30000代码 - 找一个不错的工作
+//while 循环
+int main()
+{
+	int line = 0;
+	while (line<30000)
+	{
+		printf("写代码:%d\n", line);
+		line++;
+	}
+	if (line == 30000)
+	{
+		printf("好offer");
+	}
+	
+	return 0;
+
+}
