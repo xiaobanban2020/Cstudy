f943b86a11fafc9b2d8c220deca964523ad00d56
xiaobanban2020 <panmz1990@126.com>
2022-11-24 16:06:14 +0800

    add function

---
+++ function_20221122/function_20221122/test.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int Add(int x, int y)
+//{
+//	int z = 0;
+//	z = x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	scanf("%d%d",&num1,&num2);
+//	//int sum = num1 + num2;
+//	//函数实现
+//	int sum = Add(num1,num2);
+//	printf("sum=%d",sum);
+//	return 0;
+//}
+
+//数组- 一组相同类型的元素的集合
+//10个整型1-10存起来
+int main()
+{
+	//int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int i = 0;
+	//while (i < 10)
+	//{
+	//	printf("%d ", arr[i]);
+	//	i++;
+	//}
+	//char ch[5] = { 'a','b','c' };//不完全初始化，剩余的默认为0
+}
